1b1e494262a00c6dc1d1ef111b495e1e0d7e53f6
pavanrishi <pavanrishi1432@gmail.com>
2023-07-08 20:25:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+ // Update the path to the RouterDOM file
+import Usermanagement from './Components/Usermanagement';
+
+function App ()  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+       
+        <Route path="/" element={<Usermanagement />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
